0cce37582f976b52c41c04c44a568d4f4ed1feb5
tkdy123 <tkdy006@gmail.com>
2025-02-14 09:49:27 +0900

    20250214 Spring init

---
+++ boot01/src/main/java/com/yedam/app/mybatis/service/EmpInfo.java
@@ -0,0 +1,19 @@
+package com.yedam.app.mybatis.service;
+
+import java.util.Date;
+
+import lombok.Data;
+
+// 값을 담는 객체 : VO 클래스
+@Data
+public class EmpInfo {
+	//private int employee_id; // employeeId 
+    //private String last_name;
+    //private Date hire_date;
+    //private double salary;
+	
+	private int employeeId; 
+    private String lastName;
+    private Date hireDate;
+    private double salary;
+}
