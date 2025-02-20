11351b386cdde9c3fa85fcdef57c1658889d7247
tkdy123 <tkdy006@gmail.com>
2025-02-20 11:10:42 +0900

    20250220 boot

---
+++ boot02/src/main/java/com/yedam/app/board/service/BoardVO.java
@@ -0,0 +1,16 @@
+package com.yedam.app.board.service;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class BoardVO {
+	private Integer bno;     // 번호
+	private String title;    // 제목
+	private String contents; // 내용
+	private String writer;   // 작성자
+	private Date regdate;    // 작성일
+	private Date updatedate; // 수정일
+	private String image;    // 첨부이미지
+}
