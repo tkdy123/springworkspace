553f7eafac9b19109e6f5fefdc0efbe6e8d0897a
tkdy123 <tkdy006@gmail.com>
2025-02-20 09:20:03 +0900

    20250219 boot02

---
+++ boot02/src/main/java/com/yedam/app/Boot02Application.java
@@ -0,0 +1,14 @@
+package com.yedam.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class Boot02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot02Application.class, args);
+	}
+
+}
